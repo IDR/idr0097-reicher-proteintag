067077ed6ccd0b845497c7e6127301d80c32717b
Sebastien Besson <seb.besson@gmail.com>
2020-09-30 15:53:00 +0100

    Update conversion script to convert entire submission

--- scripts/convert_patterns.sh
+++ scripts/convert_patterns.sh
@@ -6,7 +6,7 @@ mkdir -p /data/${today}-raw/sequencing
 mkdir -p /data/${today}-ometiff/timelapse
 mkdir -p /data/${today}-ometiff/sequencing
 
-for i in 00; do
+for i in 00 01 02 03 04 05 06 07 08; do
     /opt/bioformats2raw/bin/bioformats2raw /data/${today}-patterns/timelapse/tile${i}.pattern /data/${today}-raw/timelapse/tile${i}
     /opt/bioformats2raw/bin/bioformats2raw /data/${today}-patterns/sequencing/tile${i}.pattern /data/${today}-raw/sequencing/tile${i}
     /opt/raw2ometiff/bin/raw2ometiff /data/${today}-raw/timelapse/tile${i} /data/${today}-ometiff/timelapse/tile${i}.ome.tiff
--- scripts/make_patterns.sh
+++ scripts/make_patterns.sh
@@ -6,18 +6,18 @@ mkdir ${today}-patterns -p
 mkdir ${today}-patterns/timelapse -p
 mkdir ${today}-patterns/sequencing -p
 
-for i in 00; do
+for i in 00 01 02 03 04 05 06 07 08; do
     for j in 0h 3h; do
-        (cd ${today}-patterns/timelapse && ln -s ../../original/${j}_green_tile${i}.tif tile${i}_t${j}.tif)
+        (cd ${today}-patterns/timelapse && ln -s ../../20200929-ftp/ExperimentB/${j}_green_tile${i}.tif tile${i}_t${j}.tif)
     done
     echo "tile${i}_t<0h,3h>.tif" > ${today}-patterns/timelapse/tile${i}.pattern
 done
 
 cycles=(SBS1_red SBS1_yellow SBS2_red SBS2_yellow SBS3_red SBS3_yellow SBS4_red SBS4_yellow SBS5_red SBS5_yellow SBS6_red SBS6_yellow SBS7_red SBS7_yellow SBS7_DAPI)
 
-for i in 00; do
+for i in 00 01 02 03 04 05 06 07 08; do
     for j in "${!cycles[@]}"; do
-        (cd ${today}-patterns/sequencing  && ln -s ../../original/${cycles[j]}_tile${i}.tif tile${i}_c${j}.tif)
+        (cd ${today}-patterns/sequencing  && ln -s ../../20200929-ftp/ExperimentB/${cycles[j]}_tile${i}.tif tile${i}_c${j}.tif)
     done
     echo "tile${i}_c<0-14>.tif" > ${today}-patterns/sequencing/tile${i}.pattern
 done
