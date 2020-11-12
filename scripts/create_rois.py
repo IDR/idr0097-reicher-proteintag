25dbde12a428bddb50aa8e4d32ff0e80979c009f
Sebastien Besson <seb.besson@gmail.com>
2020-11-12 13:17:54 +0000

    Update create_rois.py to create per-image ROI tables

--- scripts/create_rois.py
+++ scripts/create_rois.py
@@ -2,36 +2,41 @@
 
 import argparse
 import pandas
-import omero
 from omero.cli import cli_login
 from omero.gateway import BlitzGateway
 from omero.model import PointI, RoiI, ImageI
-from omero.rtypes import rdouble, rint, rstring
+from omero.rtypes import rdouble, rstring
 
 import sys
 
-PROCESSED_FILE_PATH =  (
+PROCESSED_FILE_PATH = (
     "/uod/idr/filesets/idr0097-reicher-proteintag/20200929-ftp/"
     "experimentB-processed.txt")
 
 
 def create_points(conn, df, image):
 
-    name = image.getName()
-    iid = image.getId()
-    x = [float(x) for x in df[df['Source Name'] == name]['Location_X']]
-    y = [float(x) for x in df[df['Source Name'] == name]['Location_Y']]
-    text = list(df[df['Source Name'] == name]['Tagged Protein'])
-    
-    for i, j, k in zip(x, y, text):
-        p = omero.model.PointI()
-        p.x = rdouble(i)
-        p.y = rdouble(j)
-        p.setTextValue(rstring(k))
-        roi = omero.model.RoiI()    
+    columns = [
+        "Sequence",
+        "Tagged Protein",
+        "Sequence is unique",
+        "Location_X",
+        "Location_Y"
+    ]
+    df2 = df.DataFrame(columns=(['roi'] + columns))
+    index = df['Source Name'] == image.getName()
+    for s, tp, u, x, y in zip(map(lambda x: df[index][x], columns)):
+        p = PointI()
+        p.x = rdouble(float(x))
+        p.y = rdouble(float(y))
+        p.setTextValue(rstring(tp))
+        roi = RoiI()
         roi.addShape(p)
-        roi.setImage(omero.model.ImageI(iid, False))
-        conn.getUpdateService().saveAndReturnObject(roi)
+        roi.setName(rstring(tp))
+        roi.setImage(ImageI(image.getId(), False))
+        roi = conn.getUpdateService().saveAndReturnObject(roi)
+        df2.loc[len(df2)] = (roi.getId().getValue(), s, tp, u, x, y)
+    return df2
 
 
 def get_timelapse_images(conn):
@@ -52,14 +57,17 @@ def main(argv):
     parser.add_argument(
         '--quiet', '-q', action='count', default=0,
         help='Decrease the command verbosity')
-    args = parser.parse_args(argv)
+    parser.parse_args(argv)
 
     df = pandas.read_csv(PROCESSED_FILE_PATH, delimiter='\t')
-    with omero.cli.cli_login() as c:
-        conn = omero.gateway.BlitzGateway(client_obj=c.get_client())
+    with cli_login() as c:
+        conn = BlitzGateway(client_obj=c.get_client())
         images = get_timelapse_images(conn)
         for image in images:
-            create_points(conn, df, image)
+            df2 = create_points(conn, df, image)
+            iid = image.getId().getValue()
+            df2.to_csv(f"{iid}.csv")
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
